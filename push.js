a467a3d98cdb72903d74d47ac542e168f6bf6f98
Dvacher <aleksandr.n.petrov@gmail.com>
2020-11-27 20:12:12 +0300

    Update push.js

--- push.js
+++ push.js
@@ -29,6 +29,23 @@ if ('Notification' in window && 'serviceWorker' in navigator && 'localStorage' i
             }
         });
     });
+    
+    self.addEventListener('notificationclick', function(event) {
+        const target = event.notification.data.click_action || '/';
+        event.notification.close();
+        event.waitUntil(clients.matchAll({
+            type: 'window',
+            includeUncontrolled: true
+        }).then(function(clientList) {
+            for (var i = 0; i < clientList.length; i++) {
+                var client = clientList[i];
+                if (client.url === target && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            return clients.openWindow(target);
+        }));
+    });
 } else {
     console.warn('This browser does not support desktop notification.');
     console.log('Is HTTPS', window.location.protocol === 'https:');

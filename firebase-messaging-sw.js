c4eb86c3291a2d4f5e59a0d6452618b136e88fc0
Dvacher <aleksandr.n.petrov@gmail.com>
2020-11-28 21:23:06 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -22,18 +22,18 @@ messaging.setBackgroundMessageHandler(function(payload) {
 });
 
 self.addEventListener('notificationclick', function(event) {
-  const target = event.notification.data.click_action || '/';
-  event.notification.close();
-  event.waitUntil(clients.matchAll({
-    type: 'window',
-    includeUncontrolled: true
-  }).then(function(clientList) {
-    for (var i = 0; i < clientList.length; i++) {
-      var client = clientList[i];
-      if (client.url === target && 'focus' in client) {
-        return client.focus();
-      }
-    }
-    return clients.openWindow(target);
-  }));
+    const target = 'https://among2ch.ga';
+    event.notification.close();
+    event.waitUntil(clients.matchAll({
+        type: 'window',
+        includeUncontrolled: true
+    }).then(function(clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+            var client = clientList[i];
+            if (client.url === target && 'focus' in client) {
+                return client.focus();
+            }
+        }
+        return clients.openWindow(target);
+    }));
 });
